000db4c2980e10a48e736aef243065d2d0d162b5
gouthamsabbani <77073493+gouthamsabbani@users.noreply.github.com>
2021-08-10 04:34:46 +0100

    Add files via upload

---
+++ myjsfunction.js
@@ -0,0 +1,13 @@
+function changeImage()
+{ 
+   var image = document.getElementById('myImage');
+   if
+     (image.src.match("C:\Users\gouth\Desktop\bitcoin-cash-logo"))
+      {
+	image.src="C:\Users\gouth\Desktop\bit coin.jpg";
+      }
+     else
+     {
+	image.src="C:\Users\gouth\Desktop\bitcoin cash logo.jpg";
+     }
+}
